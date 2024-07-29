04159ac5d736e3d5da1db113c1b95d8bcc86dd4f
1dNDN <nlazarev@cleverence.ru>
2024-07-30 00:56:48 +0300

    Добавил аргументы

--- Bruteforce/Program.cs
+++ Bruteforce/Program.cs
@@ -6,31 +6,37 @@ using Bruteforce;
 using Bruteforce.TorrentWrapper;
 using Bruteforce.TorrentWrapper.Extensions;
 
-Console.WriteLine("Hello, World!");
-TorrentInfo.TryLoad(@"C:\Users\nikit\RiderProjects\Bruteforce\Data\orelli.torrent", out var torrent);
 
-var pieces = PersistenceManager.Verify(@"E:\Torrents\fidel_windows_3.1.2", torrent);; // start torrent file
+// DoWork(@"C:\Users\nikit\RiderProjects\Bruteforce\Data\orelli.torrent", @"E:\Torrents\fidel_windows_3.1.2");
+DoWork(Environment.GetCommandLineArgs()[1], Environment.GetCommandLineArgs()[2]);
 
-Console.WriteLine($"Похуевило {pieces.Count} частям");
-
-foreach (var piece in pieces)
+void DoWork(string pathToTorrent, string pathToDir)
 {
-    foreach (var b in piece.Hash.ToByteArrayFromHex())
+    Console.WriteLine("Hello, World!");
+    TorrentInfo.TryLoad(pathToTorrent, out var torrent);
+
+    var pieces = PersistenceManager.Verify(pathToDir, torrent);
+
+    Console.WriteLine($"Похуевило {pieces.Count} частям");
+
+    foreach (var piece in pieces)
     {
-        Console.Write($"{b} ");
+        foreach (var b in piece.Hash.ToByteArrayFromHex())
+        {
+            Console.Write($"{b} ");
+        }
+        Console.WriteLine();
+    
+        Console.WriteLine($"Хуярим часть номер {piece.Index}");
+    
+        if(!piece.Restoreable)
+            Console.WriteLine("Тут одни нули, хуйня выходит");
+    
+        var sw = Stopwatch.StartNew();
+
+        Console.WriteLine(BruteforceParallel.Bruteforce(piece.Bytes, piece.Hash.ToByteArrayFromHex()));
+
+        sw.Stop();
+        Console.WriteLine($"Elapsed: {sw.Elapsed} for {piece.Bytes.Length} bytes");
     }
-    Console.WriteLine();
-    
-    Console.WriteLine($"Хуярим часть номер {piece.Index}");
-    
-    if(!piece.Restoreable)
-        Console.WriteLine("Тут одни нули, хуйня выходит");
-    
-    var sw = Stopwatch.StartNew();
-
-    Console.WriteLine(BruteforceParallel.Bruteforce(piece.Bytes, piece.Hash.ToByteArrayFromHex()));
-
-    sw.Stop();
-    Console.WriteLine($"Elapsed: {sw.Elapsed} for {piece.Bytes.Length} bytes");
-    
 }
