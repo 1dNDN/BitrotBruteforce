475250efdc7f7dda14ecdb197fbd9803e1ce03ac
1dNDN <nlazarev@cleverence.ru>
2024-08-25 19:48:39 +0300

    Обновил CLI и добавил команды по запросу korden

--- Bruteforce/Program.cs
+++ Bruteforce/Program.cs
@@ -5,51 +5,82 @@ using System.Diagnostics;
 using Bruteforce;
 using Bruteforce.TorrentWrapper;
 using Bruteforce.TorrentWrapper.Extensions;
+using System.CommandLine;
 
 
-// DoWork(@"C:\Users\nikit\RiderProjects\Bruteforce\orelli2.torrent", @"C:\Users\nikit\RiderProjects\Bruteforce");
 
-// var args = Environment.GetCommandLineArgs();
+var torrentPath = new Argument<string>("torrent", description: "Путь к торрент-файлу");
+var dataPath = new Argument<string>("data", description: "Путь к данным");
 
-if (args.Length < 2)
-    Console.WriteLine("Ты сначала спиздани, че надо, а потом суету наводи");
+var restore = new Option<bool>("--restore", description: "Должна ли утилита самостоятельно восстановить данные");
+restore.AddAlias("-r");
 
-var pathToTorrent = args[1];
-var pathToDir = args[2];
+var bruteCommand = new Command("brute", "Найти и исправить битрот") {
+    torrentPath,
+    dataPath,
+    restore
+};
 
-var doRepair = false;
+bruteCommand.SetHandler(Worker.DoWork, torrentPath, dataPath, restore);
 
-if(args.Length == 3)
-{
-    doRepair = args[3] == "y";
-}
-
-if (args.Length == 4)
-{
-    var pieceIndex = Convert.ToInt32(args[3]);
-    var bitIndex = Convert.ToInt32(args[4]);
-    
-    Worker.DoWork(pathToTorrent, pathToDir, pieceIndex, bitIndex);
-}
-else
-{
-    Worker.DoWork(pathToTorrent, pathToDir, doRepair);
-}
+var indexOfPiece = new Argument<int>("piece-index", description: "Номер части (отсчет с нуля)");
+var indexOfBit = new Argument<int>("bit-index", description: "Номер бита (отсчет с нулевого бита части)");
 
 
+var restoreCommand = new Command("restore", "Исправить битрот с известным местом") {
+    torrentPath,
+    dataPath,
+    indexOfPiece,
+    indexOfBit
+};
+
+restoreCommand.SetHandler(Worker.DoWork, torrentPath, dataPath, indexOfPiece, indexOfBit);
+
+var destination = new Argument<string>("destination", description: "Путь к файлу с частью");
+
+var extractCommand = new Command("extract", "Вытащить из торрента часть") {
+    torrentPath,
+    dataPath,
+    destination,
+    indexOfPiece
+};
+
+extractCommand.SetHandler(Worker.Extract, torrentPath, dataPath, destination, indexOfPiece);
+
+var insertCommand = new Command("insert", "Записать в торрент часть") {
+    torrentPath,
+    dataPath,
+    destination,
+    indexOfPiece
+};
+
+extractCommand.SetHandler(Worker.Insert, torrentPath, dataPath, destination, indexOfPiece);
+
+var rootCommand = new RootCommand {
+    bruteCommand,
+    restoreCommand,
+    extractCommand,
+    insertCommand
+};
+
+rootCommand.Description = "Хуярим всякую хуйню с похуяренными жизнью торрентами";
+
+return rootCommand.Invoke(args);
+
+// https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial
 
 class Worker
 {
-    public static void DoWork(string pathToTorrent, string pathToDir, bool doRepair)
+    public static void DoWork(string torrentPath, string dataPath, bool doRepair)
     {
-        TorrentInfo.TryLoad(pathToTorrent, out var torrent);
+        TorrentInfo.TryLoad(torrentPath, out var torrent);
 
         if (torrent.Files.Length == 1 && torrent.Files[0].FilePath.Contains("Posobie_dlja_samoubijz"))
             Console.WriteLine("Нихуя себе, сегодня хуярим петуха!");
         else
             Console.WriteLine("Опять хуйню прислали");
 
-        var pieces = PersistenceManager.Verify(pathToDir, torrent);
+        var pieces = PersistenceManager.Verify(dataPath, torrent);
 
         Console.WriteLine($"Похуевило {pieces.Count} частям");
 
@@ -77,7 +108,7 @@ class Worker
 
             if (doRepair && bitIndex > 0)
             {
-                PersistenceManager.FlipBit(pathToDir, torrent, piece.Index, bitIndex);
+                PersistenceManager.FlipBit(dataPath, torrent, piece.Index, bitIndex);
             }
 
             sw.Stop();
@@ -85,16 +116,16 @@ class Worker
         }
     }
 
-    public static void DoWork(string pathToTorrent, string pathToDir, int pieceIndex, int bitIndex)
+    public static void DoWork(string torrentPath, string dataPath, int pieceIndex, int bitIndex)
     {
-        TorrentInfo.TryLoad(pathToTorrent, out var torrent);
+        TorrentInfo.TryLoad(torrentPath, out var torrent);
 
         if (torrent.Files.Length == 1 && torrent.Files[0].FilePath.Contains("Posobie_dlja_samoubijz"))
             Console.WriteLine("Нихуя себе, сегодня хуярим петуха!");
         else
             Console.WriteLine("Опять хуйню прислали");
 
-        var pieces = PersistenceManager.Verify(pathToDir, torrent);
+        var pieces = PersistenceManager.Verify(dataPath, torrent);
 
         Console.WriteLine($"Похуевило {pieces.Count} частям");
 
@@ -102,8 +133,30 @@ class Worker
         {
             Console.WriteLine($"Хуярим часть номер {piece.Index}");
 
-            PersistenceManager.FlipBit(pathToDir, torrent, piece.Index, bitIndex);
+            PersistenceManager.FlipBit(dataPath, torrent, piece.Index, bitIndex);
             Console.WriteLine("Нахуярили");
         }
+    }
+
+    public static void Extract(string torrentPath, string dataPath, string destination, int pieceIndex)
+    {
+        TorrentInfo.TryLoad(torrentPath, out var torrent);
+
+        var bytes = PersistenceManager.Get(dataPath, torrent.Files, torrent.PieceLength, pieceIndex);
+        
+        File.WriteAllBytes(destination, bytes);
+        
+        Console.WriteLine($"Успешно насрато {bytes.Length} байтов хуйни в {destination}");
+    }
+    
+    public static void Insert(string torrentPath, string dataPath, string destination, int pieceIndex)
+    {
+        TorrentInfo.TryLoad(torrentPath, out var torrent);
+        
+        var bytes = File.ReadAllBytes(destination);
+        
+        PersistenceManager.Put(dataPath, torrent.Files, torrent.PieceLength, pieceIndex, bytes);
+        
+        Console.WriteLine($"Успешно всрато {bytes.Length} байтов хуйни в {destination}");
     }
 }
