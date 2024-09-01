9ca8f629ef2dd5a0795d6edd58a586fa97316f90
1dNDN <nlazarev@cleverence.ru>
2024-09-01 20:03:27 +0300

    Удалил неактуальные бинарники

--- Bruteforce/Utility.cs
+++ Bruteforce/Utility.cs
@@ -74,66 +74,11 @@ public static class LibreSSLDesktop
     public static extern void SHA1(byte[] d, int n, byte[] md);
 }
 
-// 12,02 ГБ/с
-public static class LibreSSLOnecore
-{
-    [DllImport("libcrypto_libressl_desktop.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void SHA1(byte[] d, int n, byte[] md);
-}
-
-// 7,55 ГБ/с
-public static class LibreSSL
-{
-    [DllImport("libcrypto_libre.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void SHA1(byte[] d, int n, byte[] md);
-}
-
-// 32,74 ГБ/с
-public static class OpenSSLMingw
-{
-    [DllImport("libcrypto-3-x64_mingw_openssl.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void SHA1(byte[] d, int n, byte[] md);
-}
-
-// 31,04 ГБ/с
-public static class OpenSSLMingwNew
-{
-    [DllImport("libcrypto-3-x64_mingw_new.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void SHA1(byte[] d, int n, byte[] md);
-}
-
 // 32,71 ГБ/с
 public static class OpenSSLCl
 {
     [DllImport("libcrypto-3-x64_cl.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern void SHA1(byte[] d, int n, byte[] md);
-}
-
-// 30.52 ГБ/с
-public static class GnuTLS
-{
-    // Function: int gnutls_hash_fast (gnutls_digest_algorithm_t algorithm, const void * ptext, size_t ptext_len, void * digest)
-    // algorithm: the hash algorithm to use
-    // ptext: the data to hash
-    // ptext_len: the length of data to hash
-    // digest: is the output value of the hash
-    
-    // This convenience function will hash the given data and return output on a single call.
-    //
-    // Returns: Zero or a negative error code on error.
-
-    [DllImport("libgnutls-30_mingw", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int gnutls_hash_fast(GnuTLS_Digest_Algorithm algorithm, byte[] ptext, int ptext_len, byte[] digest);
-
-    public static int SHA1(byte[] data, int dataLength, byte[] digest)
-    {
-        return gnutls_hash_fast(GnuTLS_Digest_Algorithm.GNUTLS_DIG_SHA1, data, dataLength, digest);
-    }
-
-    public enum GnuTLS_Digest_Algorithm
-    {
-        GNUTLS_DIG_SHA1 = 3
-    }
 }
 
 public static class ThreadExtension
