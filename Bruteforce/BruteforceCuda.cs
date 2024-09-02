f5e3cf76bb2d32129c9bf5259d055b75025aad12
1dNDN <nlazarev@cleverence.ru>
2024-09-02 23:55:39 +0300

    Добавил поддержку невыровненных данных

--- Bruteforce/BruteforceCuda.cs
+++ Bruteforce/BruteforceCuda.cs
@@ -8,13 +8,6 @@ namespace Bruteforce;
 
 public partial class BruteforceCuda
 {
-    // CudaAlignedBitrotFinder.dll
-    // void __declspec(dllexport) bruteforceBits(unsigned char* pieceData, unsigned char* pieceHash, size_t pieceSize, unsigned int* result)
-    // В result попадает индекс бита, который нужно флипнуть, либо 4294967295 (-1 в unsigned) если хеш-сумма не найдена
-    [LibraryImport("CudaAlignedBitrotFinder")]
-    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    private static partial void bruteforceBits(byte[] pieceData, byte[] pieceHash, int pieceSize, ref uint result);
-
     public static int Bruteforce(byte[] data, byte[] hash)
     {
         if (IsEqual(hash, GetHash(data)))
@@ -22,11 +15,30 @@ public partial class BruteforceCuda
 
         var result = uint.MaxValue;
 
-        bruteforceBits(data, hash, data.Length, ref result);
+        if(data.Length % 64 == 0)
+            BruteforceAligned.bruteforceBits(data, hash, data.Length, ref result);
+        else 
+            BruteforceUnaligned.bruteforceBits(data, hash, data.Length, ref result);
 
         if (result == uint.MaxValue)
             return -3;
 
         return (int)result;
     }
+}
+
+
+// CudaAlignedBitrotFinder.dll
+// void __declspec(dllexport) bruteforceBits(unsigned char* pieceData, unsigned char* pieceHash, size_t pieceSize, unsigned int* result)
+// В result попадает индекс бита, который нужно флипнуть, либо 4294967295 (-1 в unsigned) если хеш-сумма не найдена
+public class BruteforceAligned
+{
+    [DllImport("CudaAlignedBitrotFinder", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void bruteforceBits(byte[] pieceData, byte[] pieceHash, int pieceSize, ref uint result);
+}
+
+public class BruteforceUnaligned
+{
+    [DllImport("CudaUnalignedBitrotFinder", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void bruteforceBits(byte[] pieceData, byte[] pieceHash, int pieceSize, ref uint result);
 }
